094364541b20a1cd28eed7388bca0b26c97262ca
diegop1zarro <diego.cab.1016@gmail.com>
2022-09-14 17:23:32 -0300

    Error en texto

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
         <h2>Un poco sobre mi</h2>
             <h3>Diego Pizarro</h3>
         <div className="About_MContainer">
-        <p>Hola mi nombre es Diego y soy desarrollador web Full Stack, mis conocimientos de los debo a Henry, un bootcamp intensivo el cúal no solo me enseño de tecnologías si no a nunca rendirme, a pensar en positivo y que todo tiene una solución, si usted está buscando una persona que sea responsable, apasionada y que le guste hacer amigos, no dude en contactarme.
+        <p>Hola mi nombre es Diego y soy desarrollador web Full Stack, mis conocimientos se los debo a Henry, un bootcamp intensivo el cúal no solo me enseño de tecnologías si no a nunca rendirme, a pensar en positivo y que todo tiene una solución, si usted está buscando una persona que sea responsable, apasionada y que le guste hacer amigos, no dude en contactarme.
           <div>
           Email: diego.cab.1016@gmail.com
           </div>
