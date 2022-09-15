90114e061733fc0098c022e314c69cb053403fec
diegop1zarro <diego.cab.1016@gmail.com>
2022-09-14 21:43:56 -0300

    email js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import logo from './logo.svg';
 import { Route, Routes } from "react-router-dom";
-// import SideBar from './Components/SideBar/SideBar.jsx';
-// import About from './Components/About/About';
-import All from './Components/All/All';
-// import Carousel from './Components/Carousel/Carousel.jsx'
+import All from './Components/All/All'; 
 import './App.css';
 
 function App() {
@@ -11,8 +8,6 @@ function App() {
     <div className="App">
     {/* <SideBar/> */}
      <Routes>
-      {/* <Route path="" element={<About/>}/> */}
-      {/* <Route path="" element={<Carousel/>}/> */}
       <Route path="" element={<All/>}/> 
     </Routes>
     </div>
