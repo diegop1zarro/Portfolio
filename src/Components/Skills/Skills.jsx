9a3f8a95da0d7e1504065e9cf2ecfb05a13f2e86
diegop1zarro <diego.cab.1016@gmail.com>
2022-09-16 05:19:10 -0300

    fade skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -37,7 +37,7 @@ const Skills = () => {
       <Swing>
             <h2>Tecnologías</h2>
       </Swing>
-      <Fade delay={1500} top cascade>
+      <Fade delay={1200} top cascade>
             <div className='container_logos'>
                 {logos.map((k, i) => (
                     <div className='logo' key={i}>
