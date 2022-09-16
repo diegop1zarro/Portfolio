81e536a62bc90b8fbc192f18fb8858bbae2ce859
diegop1zarro <diego.cab.1016@gmail.com>
2022-09-16 05:17:09 -0300

    All titulo principal

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
             <h3>Diego Pizarro</h3>
         <div className="About_MContainer">
         <p>
-          <Fade delay={1500} top cascade>
+          <Fade delay={1200} top cascade>
           Hola mi nombre es Diego y soy desarrollador web Full Stack, mis conocimientos se los debo a Henry, un bootcamp intensivo el cúal no solo me enseño de tecnologías si no a nunca rendirme, a pensar en positivo y que todo tiene una solución, si usted está buscando una persona que sea responsable, apasionada y que le guste hacer amigos, no dude en contactarme.
           <div>
           Email: diego.cab.1016@gmail.com
--- src/Components/All/All.jsx
+++ src/Components/All/All.jsx
@@ -3,18 +3,7 @@ import About from '../About/About.jsx';
 import Skills from '../Skills/Skills.jsx';
 import Projects from '../Projects/Projects.jsx';
 import Contact from '../Contact/Contact.jsx';
-// import Fade from 'react-reveal/Fade';
-// import Jello from 'react-reveal/Jello';
-// import HeadShake from 'react-reveal/HeadShake'; // se mueve un poco de izq a der
-// import Pulse from 'react-reveal/Pulse';  // se agranda y se achica
-import RubberBand from 'react-reveal/RubberBand'; // tambien pero con mas flow
-// import Shake from 'react-reveal/Shake'; // se bate para los costado
-// import Spin from 'react-reveal/Spin'; // una locura gira como un espiner
-// import Swing from 'react-reveal/Swing'; // me gusta 10/10
-// import Tada from 'react-reveal/Tada'; // ta hueno
-// import Wobble from 'react-reveal/Wobble'; // muy exagerado
-// import Slide from 'react-reveal/Slide';
-// import LightSpeed from 'react-reveal/LightSpeed'; // ta hueno
+import RubberBand from 'react-reveal/RubberBand'; 
 import './All.css'
 export default function All() {
   return (
